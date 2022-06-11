c0e9003cc16692bfa85fe0bcbf7a81086c59d2cf
Christopher Carrick <my396car@gmail.com>
2022-06-11 09:48:07 -0700

    Optimize build process

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+
 const path = require('path')
 const { ESBuildMinifyPlugin } = require('esbuild-loader')
 const CopyPlugin = require('copy-webpack-plugin')
 
 module.exports = {
-  entry: path.join(__dirname, 'ts', 'index.ts'),
+  entry: [path.join(__dirname, 'ts', 'index.ts'), path.join(__dirname, 'scss', 'style.scss')],
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'index.js'
@@ -25,9 +27,21 @@ module.exports = {
         test: /.pug/,
         exclude: /node_modules/,
         use: ['pug-loader']
+      },
+      {
+        test: /\.s[ac]ss$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: { outputPath: 'static/', name: '[name].css' }
+          },
+          'sass-loader'
+        ]
       }
     ]
   },
+  devtool: process.env.NODE_ENV == 'development' ? 'inline-source-map' : false,
   plugins: [new CopyPlugin({ patterns: [{ from: './assets/', to: './static/' }] })],
   optimization: {
     minimizer: [new ESBuildMinifyPlugin({ target: 'es2021', drop: ['console'] })]
